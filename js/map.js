83d111a4829d4cdc6e9e607b8888ac82882eccdd
Anastasia Titova <Kutsanastasia@yandex.ru>
2020-07-03 17:58:31 +0700

    перемещение главной метки по карте

--- js/map.js
+++ js/map.js
@@ -4,11 +4,16 @@
   var MAIN_MARKER_WIDTH = 62;
   var MAIN_MARKER_HEIGHT = 62;
   var MAIN_MARKER_TIP_HEIGHT = 22;
+  var MAP_START_Y = 130;
+  var MAP_END_Y = 630;
+  var MAP_START_X = 0;
+  var MAP_END_X = 1200;
   var LEFT_MOUSE_BUTTON = 1;
   var ENTER = 13;
 
   var map = document.querySelector('.map');
   var mainPin = map.querySelector('.map__pin--main');
+  var mapPins = map.querySelector('.map__pins');
 
   // переводим страницу в неактивное состояние
 
@@ -70,61 +75,86 @@
 
   // перемещение основной метки по карте
 
-  // (function () {
-  //   var pinMain = map.querySelector('.map__pin--main');
+  mainPin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
 
-  //   pinMain.addEventListener('mousedown', function (evt) {
-  //     evt.preventDefault();
+    // начальные координаты
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
 
-  //     // начальные координаты
-  //     var startCoords = {
-  //       x: evt.clientX,
-  //       y: evt.clientY
-  //     };
+    var dragged = false;
 
-  //     var dragged = false;
+    // функция передвижения пина
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
 
-  //     // функция передвижения пина
-  //     var onMouseMove = function (moveEvt) {
-  //       moveEvt.preventDefault();
-  //       dragged = true;
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
 
-  //       var shift = {
-  //         x: startCoords.x - moveEvt.clientX,
-  //         y: startCoords.y - moveEvt.clientY
-  //       };
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
 
-  //       startCoords = {
-  //         x: moveEvt.clientX,
-  //         y: moveEvt.clientY
-  //       };
+      var newPinMainPosition = {
+        x: mainPin.offsetLeft - shift.x,
+        y: mainPin.offsetTop - shift.y
+      };
 
-  //       map.style.top = (map.offsetTop - shift.y) + 'px';
-  //       map.style.left = (map.offsetLeft - shift.x) + 'px';
+      function getNewCoords(coordinates) {
 
-  //     };
+        // Проверка границ перемещения пина
+        var minY = MAP_START_Y - (MAIN_MARKER_HEIGHT + MAIN_MARKER_TIP_HEIGHT);
+        var maxY = MAP_END_Y - (MAIN_MARKER_HEIGHT + MAIN_MARKER_TIP_HEIGHT);
 
-  //     // функция установки пина при отпускании кнопки мыши
-  //     var onMouseUp = function (upEvt) {
-  //       upEvt.preventDefault();
+        if (coordinates.x < MAP_START_X) {
+          coordinates.x = MAP_START_X;
+        } else if (coordinates.x > MAP_END_X) {
+          coordinates.x = MAP_END_X;
+        }
 
-  //       document.removeEventListener('mousemove', onMouseMove);
-  //       document.removeEventListener('mouseup', onMouseUp);
+        if (coordinates.y < minY) {
+          coordinates.y = minY;
+        } else if (coordinates.y > maxY) {
+          coordinates.y = maxY;
+        }
 
-  //       if (dragged) {
-  //         var onClickPreventDefault = function (clickEvt) {
-  //           clickEvt.preventDefault();
-  //           pinMain.removeEventListener('click', onClickPreventDefault);
-  //         };
-  //         pinMain.addEventListener('click', onClickPreventDefault);
-  //       }
-  //     };
+        return coordinates.x + ', ' + coordinates.y;
+      }
 
-  //     document.addEventListener('mousemove', onMouseMove);
-  //     document.addEventListener('mouseup', onMouseUp);
-  //   });
+      var newCoords = getNewCoords(newPinMainPosition);
 
-  // })();
+      mainPin.style.left = newPinMainPosition.x + 'px';
+      mainPin.style.top = newPinMainPosition.y + 'px';
+      window.form.fillAddress(newCoords);
+
+    };
+
+    // функция установки пина при отпускании кнопки мыши
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          mainPin.removeEventListener('click', onClickPreventDefault);
+        };
+        mainPin.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
 
   window.map = {
     activateMap: activateMap,
